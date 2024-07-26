800d13149dc80f7e1a0759dbf8a1150d315edb27
TiborKopca <tibor.kopca@gmail.com>
2024-07-26 11:21:50 +0200

    visual changes

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 # Veterinary Appointment Booking WebApp
-### Practice of the Javascript, HTML, Bootstrap 
+### Practice of the Javascript, HTML, Tailwind CSS
 
 ## Description & Functionality
 * using js Object literal, Classes and Methods
 * validation of all input fields of the form for empty values, and date validation (date must be in the future)
 * using local storage to store data to show the data onload
+* using event listeners to add, edit and delete appointments
 * update local storage data on edit of the appointment
 * dynamically generating html elements and messages
-* using bootstrap classes to style alert messages
-
-* personalized data atributes (data-id)
+* using Tailwind classes to style alert messages
 
 ## Screenshot
 >Final Online Website Screenshot
-![here](/screenshots/screenshot.png)
+![here](./img/screenshots/screenshot.png)
 
 ## Online Link
-[github Pages Link](https://tiborkopca.github.io/)
+[github Pages Link](https://tiborkopca.github.io/Javascript2024-Vet-Appointment-App/)
 
 ## Authors
 
--- js/app.js
+++ js/app.js
@@ -6,9 +6,11 @@ const fechaInput = document.querySelector("#fecha");
 const sintomasInput = document.querySelector("#sintomas");
 
 const formulario = document.querySelector("#formulario-cita");
+//Selector for the submit button
 const formularioInput = document.querySelector(
   '#formulario-cita input[type="submit"]'
 );
+//Selector for the div container of the appointments
 const contenedorCitas = document.querySelector("#citas");
 
 //EVENTS
@@ -22,6 +24,7 @@ formulario.addEventListener("submit", submitCita);
 
 document.addEventListener("DOMContentLoaded", cargarDatosLocalStorage);
 
+//AUXILIARY VARIABLE - EDITING
 let editando = false;
 
 //OBJECT LITERAL OF APPOINTMENT
@@ -87,9 +90,12 @@ class AdminCitas {
     this.citas = [...this.citas, cita];
     this.mostrar();
   }
-
+  //EDIT EXISTING APPOINTMENT, FIND THE APPOINTMENT OBJECT PASSED, WITH THE ID PROPERTY
   editar(citaActualizada) {
+    //map iterates over each element of the array, and returns a new array with the result of calling the callback function on each element
     this.citas = this.citas.map((cita) =>
+      //for each element of the array, we check if the id of the element is the same as the id of the object passed and show it in the DOM with 'mostrar()'
+      //if its not the same, we return the element of the array(each element is an object in the array)
       cita.id === citaActualizada.id ? citaActualizada : cita
     );
     this.mostrar();
@@ -100,21 +106,21 @@ class AdminCitas {
     //add code to remove the object from Local Storage
     this.mostrar();
   }
-
+  //SHOW THE APPOINTMENTS IN THE DOM
   mostrar() {
-    // Limpiar el HTML
+    //CLEAR THE HTML
     while (contenedorCitas.firstChild) {
       contenedorCitas.removeChild(contenedorCitas.firstChild);
     }
 
-    // Si hay citas
+    //THERE ARE NO APPOINTMENTS => SHOW A MESSAGE
     if (this.citas.length === 0) {
       contenedorCitas.innerHTML =
         '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>';
       return;
     }
 
-    // Generando las citas
+    //GENERATE THE APPOINTMENTS from the array
     this.citas.forEach((cita) => {
       const divCita = document.createElement("div");
       divCita.classList.add(
@@ -133,48 +139,49 @@ class AdminCitas {
       paciente.classList.add(
         "font-normal",
         "mb-3",
-        "text-gray-700",
+        "text-white",
         "normal-case"
       );
-      paciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${cita.paciente}`;
+      paciente.innerHTML = `<span class="font-bold text-gray-700 uppercase">Paciente: </span> ${cita.paciente}`;
 
       const propietario = document.createElement("p");
       propietario.classList.add(
         "font-normal",
         "mb-3",
-        "text-gray-700",
+        "text-white",
         "normal-case"
       );
-      propietario.innerHTML = `<span class="font-bold uppercase">Propietario: </span> ${cita.propietario}`;
+      propietario.innerHTML = `<span class="font-bold text-gray-700 uppercase">Propietario: </span> ${cita.propietario}`;
 
       const email = document.createElement("p");
       email.classList.add(
         "font-normal",
         "mb-3",
-        "text-gray-700",
+        "text-white",
         "normal-case"
       );
-      email.innerHTML = `<span class="font-bold uppercase">E-mail: </span> ${cita.email}`;
+      email.innerHTML = `<span class="font-bold text-gray-700 uppercase">E-mail: </span> ${cita.email}`;
 
       const fecha = document.createElement("p");
       fecha.classList.add(
         "font-normal",
         "mb-3",
-        "text-gray-700",
+        "text-white",
         "normal-case"
       );
-      fecha.innerHTML = `<span class="font-bold uppercase">Fecha: </span> ${cita.fecha}`;
+      fecha.innerHTML = `<span class="font-bold text-gray-700 uppercase">Fecha: </span> ${cita.fecha}`;
 
       const sintomas = document.createElement("p");
       sintomas.classList.add(
         "font-normal",
         "mb-3",
-        "text-gray-700",
+        "text-white",
         "normal-case"
       );
-      sintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${cita.sintomas}`;
+      sintomas.innerHTML = `<span class="font-bold text-gray-700 uppercase">Síntomas: </span> ${cita.sintomas}`;
 
-      // Botones de Eliminar y editar
+      //BUTTONS TO DELETE AND EDIT
+      //EDIT BUTTON FOR THE APPOINTMENT
       const btnEditar = document.createElement("button");
       btnEditar.classList.add(
         "py-2",
@@ -192,9 +199,14 @@ class AdminCitas {
       );
       btnEditar.innerHTML =
         'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
+
+      //EVENT HANDLER FOR THE EDIT BUTTON ON THE DYNAMICALLY CREATED BUTTON, for each appointment, we clone the object and pass it to the function cargarEdicion
+      //without cloning the object, the object data wont be passed to the function cargarEdicion
+      //it should work with spread operator too
       const clone = structuredClone(cita);
       btnEditar.onclick = () => cargarEdicion(clone);
 
+      //DELETE BUTTON WITH THE APPOINTMENT ID
       const btnEliminar = document.createElement("button");
       btnEliminar.classList.add(
         "py-2",
@@ -209,23 +221,28 @@ class AdminCitas {
         "items-center",
         "gap-2"
       );
+
       btnEliminar.innerHTML =
-        'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+        'Eliminar <svg fill="none" class="h-5 w-5" viewBox="0 0 24 24"  stroke="white" stroke-width="2" stroke-linejoin="round"><path d="M12.255 10L18.783 3.47251C18.9311 3.32445 19.0485 3.14867 19.1287 2.95521C19.2088 2.76176 19.2501 2.55441 19.2501 2.34501C19.2501 2.13562 19.2088 1.92827 19.1287 1.73482C19.0485 1.54136 18.9311 1.36558 18.783 1.21751C18.635 1.06945 18.4592 0.951997 18.2657 0.871864C18.0723 0.791732 17.8649 0.750488 17.6555 0.750488C17.4461 0.750488 17.2388 0.791732 17.0453 0.871864C16.8519 0.951997 16.6761 1.06945 16.528 1.21751L10 7.74501L3.47253 1.21751C3.17349 0.918482 2.76792 0.750488 2.34503 0.750488C1.92213 0.750488 1.51656 0.918482 1.21753 1.21751C0.918494 1.51655 0.7505 1.92212 0.7505 2.34501C0.7505 2.76791 0.918494 3.17348 1.21753 3.47251L7.74503 10L1.21703 16.5275C1.06896 16.6756 0.951508 16.8514 0.871376 17.0448C0.791244 17.2383 0.75 17.4456 0.75 17.655C0.75 17.8644 0.791244 18.0718 0.871376 18.2652C0.951508 18.4587 1.06896 18.6344 1.21703 18.7825C1.36509 18.9306 1.54087 19.048 1.73433 19.1282C1.92778 19.2083 2.13513 19.2495 2.34453 19.2495C2.55392 19.2495 2.76127 19.2083 2.95472 19.1282C3.14818 19.048 3.32396 18.9306 3.47203 18.7825L10 12.256L16.5275 18.7835C16.839 19.095 17.2475 19.2505 17.655 19.2505C18.0625 19.2505 18.4715 19.095 18.783 18.7835C19.0819 18.4844 19.2499 18.0789 19.2499 17.656C19.2499 17.2332 19.0819 16.8276 18.783 16.5285L12.255 10Z"></path></svg>';
+
+      //DELETE APPOINTMENT BUTTON for the appointment with the id of the object
       btnEliminar.onclick = () => this.eliminar(cita.id);
 
       const contenedorBotones = document.createElement("DIV");
       contenedorBotones.classList.add("flex", "justify-between", "mt-10");
 
+      //ADDING THE BUTTONS TO THE BUTTON CONTAINER
       contenedorBotones.appendChild(btnEditar);
       contenedorBotones.appendChild(btnEliminar);
 
-      // Agregar al HTML
+      //ADD THE DATA THE DATA TO THE APPOINTMENT
       divCita.appendChild(paciente);
       divCita.appendChild(propietario);
       divCita.appendChild(email);
       divCita.appendChild(fecha);
       divCita.appendChild(sintomas);
       divCita.appendChild(contenedorBotones);
+      //ADD THE APPOINTMENT TO THE APPOINTMENTS CONTAINER
       contenedorCitas.appendChild(divCita);
     });
   }
@@ -250,16 +267,16 @@ function submitCita(e) {
   e.preventDefault();
 
   //FORM VALIDATION USING OBJECT DESTRUCTURING
-//   const { paciente, propietario, email, fecha, sintomas } = citaObj;
-//   if (!paciente || !propietario || !email || !fecha || !sintomas) {
-//     new Notificacion({
-//       texto: "Todos los campos son obligatorios(destructuring)",
-//       tipo: "error",
-//     });
-//     return;
-//   }
+  //   const { paciente, propietario, email, fecha, sintomas } = citaObj;
+  //   if (!paciente || !propietario || !email || !fecha || !sintomas) {
+  //     new Notificacion({
+  //       texto: "Todos los campos son obligatorios(destructuring)",
+  //       tipo: "error",
+  //     });
+  //     return;
+  //   }
 
-//VALIDATION OF ALL INPUT FIELDS OF THE FORM WITH .some() ==> returns true/false
+  //VALIDATION OF ALL INPUT FIELDS OF THE FORM WITH .some() ==> returns true/false
   if (Object.values(citaObj).some(validateEmpty)) {
     new Notificacion({
       texto: "Todos los campos son obligatorios",
@@ -268,13 +285,13 @@ function submitCita(e) {
     return;
   }
 
-//VALIDATION OF THE DATE FIELD
-let fechaActual = new Date()
-// console.log(`Datenow: ${(fechaActual.setHours(0,0,0,0))}`) //Datenow: 1721907603076
-let fechaDeseada = new Date(citaObj.fecha)
-// console.log(`Fecha: ${fechaDeseada.setHours(0,0,0,0)}`) //Fecha: 1721080800000
+  //VALIDATION OF THE DATE FIELD
+  let fechaActual = new Date();
+  // console.log(`Datenow: ${(fechaActual.setHours(0,0,0,0))}`) //Datenow: 1721907603076
+  let fechaDeseada = new Date(citaObj.fecha);
+  // console.log(`Fecha: ${fechaDeseada.setHours(0,0,0,0)}`) //Fecha: 1721080800000
 
-  if(fechaActual > fechaDeseada){
+  if (fechaActual > fechaDeseada) {
     new Notificacion({
       texto: "La fecha puede ser creada solo en el futuro",
       tipo: "error",
@@ -282,13 +299,16 @@ let fechaDeseada = new Date(citaObj.fecha)
     return;
   }
 
+  //EDITANDO carries boolean value, if true, the appointment is edited, if false, the appointment is newly added
   if (editando) {
+    //ADD THE COPY OF THE OBJECT OF THE APPOINTMENT TO THE APPOINTMENTS ARRAY, AND WE LATER CLEAR THE 'citaObj' OBJECT
     citas.editar({ ...citaObj });
     new Notificacion({
       texto: "Guardado Correctamente",
       tipo: "exito",
     });
   } else {
+    //ADD THE COPY OF THE OBJECT OF THE APPOINTMENT TO THE APPOINTMENTS ARRAY, AND WE LATER CLEAR THE 'citaObj' OBJECT
     citas.agregar({ ...citaObj });
     new Notificacion({
       texto: "Paciente Registrado",
@@ -298,18 +318,19 @@ let fechaDeseada = new Date(citaObj.fecha)
 
   //CLEARING THE FORM
   formulario.reset();
-  
+
   //STORE DATA IN LOCAL STORAGE
   guardarDatosLocalStorage();
-
+  //CLEARING THE OBJECT OF THE APPOINTMENT
   reiniciarObjetoCita();
 
+  //BUTTON TEXT CHANGES TO REGISTRAR PACIENTE
   formularioInput.value = "Registrar Paciente";
   editando = false;
 }
 //VALIDATION FUNCTIONS
-function validateEmpty(valor){
-    return valor.trim() === "";
+function validateEmpty(valor) {
+  return valor.trim() === "";
 }
 
 //CLEAR OBJECT
@@ -322,6 +343,8 @@ function reiniciarObjetoCita() {
   // citaObj.fecha = '';
   // citaObj.sintomas = '';
 
+  //RESETTING THE OBJECT OF THE APPOINTMENT with Object.assign , we copy empty values to the object
+  //THE id is generated with generarId() for the next appointment
   Object.assign(citaObj, {
     id: generarId(),
     paciente: "",
@@ -332,10 +355,12 @@ function reiniciarObjetoCita() {
   });
 }
 
+//GENERATING A RANDOM ID FOR THE APPOINTMENT - random math function, 36 base, substring(will delete first 2 digits), date.now, concatenate and parse to string
 function generarId() {
   return Math.random().toString(36).substring(2) + Date.now();
 }
 
+//EDITING THE APPOINTMENT, the cita object is passed to the function from the button event handler
 function cargarEdicion(cita) {
   Object.assign(citaObj, cita);
 
@@ -344,9 +369,9 @@ function cargarEdicion(cita) {
   emailInput.value = cita.email;
   fechaInput.value = cita.fecha;
   sintomasInput.value = cita.sintomas;
-
+//SET AUX VARIABLE TO TRUE - TO EXECUTE EDIT CODE IN THE FUNCTION 'submitCita'
   editando = true;
-
+  //BUTTON TEXT CHANGES TO SAVE CHANGES
   formularioInput.value = "Guardar Cambios";
 }
 
